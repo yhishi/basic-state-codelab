788e2d14e0d81488e5e267f97b9084b2d8a9626c
yhishi <ev422119.s.okayama@gmail.com>
2023-11-05 17:24:43 +0900

    WellnessScreen composable作成

--- app/src/main/java/com/example/basic/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basic/basicstatecodelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basic.basicstatecodelab.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +21,23 @@ class MainActivity : ComponentActivity() {
             BasicStateCodelabTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-
+                    WellnessScreen()
                 }
             }
         }
     }
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
 }
