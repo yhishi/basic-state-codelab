97a304f4ca2ecbe735f5e44fffbd3ab837a860d1
yhishi <ev422119.s.okayama@gmail.com>
2023-11-05 17:50:32 +0900

    remember APIを使って再コンポーズ後も状態を保持する

--- app/src/main/java/com/example/basic/basicstatecodelab/MainActivity.kt
+++ app/src/main/java/com/example/basic/basicstatecodelab/MainActivity.kt
@@ -12,7 +12,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,11 +44,11 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
         // Changes to count are now tracked by Compose
-        val count: MutableState<Int> = mutableStateOf(0)
+        var count by remember { mutableStateOf(0) }
 
-        Text("You've had ${count.value} glasses.")
+        Text("You've had ${count} glasses.")
         Button(onClick = {
-            count.value++
+            count++
         }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
